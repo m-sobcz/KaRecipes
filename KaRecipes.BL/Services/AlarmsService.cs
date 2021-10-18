3e9c4153ba90fa3aed3fc5be444ba2f692f829f0
MSobczak <m.sobcz@gmail.com>
2021-10-18 11:04:15 +0200

    AlarmsService tests

--- KaRecipes.BL/Services/AlarmsService.cs
+++ KaRecipes.BL/Services/AlarmsService.cs
@@ -11,6 +11,7 @@ namespace KaRecipes.BL.Services
     public class AlarmsService : IObserver
     {
         IPlcDataAccess plcDataAccess;
+        public Dictionary<string, Alarm> Alarms { get; private set; }
         public int PublishingInterval => 1000;
 
         public AlarmsService(IPlcDataAccess plcDataAccess)
@@ -18,15 +19,17 @@ namespace KaRecipes.BL.Services
             this.plcDataAccess = plcDataAccess;
         }
 
-        public void Subscribe(Dictionary<string, Alarm> alarms)
+        public void Start(Dictionary<string, Alarm> alarms)
         {
+            this.Alarms = alarms;
             List<string> keys = alarms.Keys.ToList();
             plcDataAccess.CreateSubscriptionsWithInterval(keys, PublishingInterval, this);
         }
 
         public void Update(PlcDataReceivedEventArgs subject)
         {
-            throw new NotImplementedException();
+            Alarms.TryGetValue(subject.Name, out Alarm alarm);
+            alarm.Value = subject.Value;
         }
     }
 }
---
+++ KaRecipes.BLTests/Services/AlarmsServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using KaRecipes.BL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KaRecipes.BL.Interfaces;
+using Moq;
+using KaRecipes.BL.AlarmAggregate;
+
+namespace KaRecipes.BL.Services.Tests
+{
+    public class AlarmsServiceTests
+    {
+
+
+        [Fact()]
+        public void Update_SampleData_UpdatesValue()
+        {
+            //Arrange
+            Mock<IPlcDataAccess> mockPlcDataAccess = new Mock<IPlcDataAccess>();
+            Dictionary<string, Alarm> alarms = new();
+            alarms.Add("KaRecipes.M01.DB_00_Parameters.single1", new Alarm() {Value=false });
+            alarms.Add("KaRecipes.M01.DB_00_Parameters.single2", new Alarm() { Value = false });
+            alarms.Add("KaRecipes.M01.DB_00_Parameters.single3", new Alarm() { Value = false });
+            AlarmsService alarmsService = new(mockPlcDataAccess.Object);
+            //Act
+            alarmsService.Start(alarms);
+            alarmsService.Update(new PlcDataReceivedEventArgs() { Name = "KaRecipes.M01.DB_00_Parameters.single2",Value=true });
+            var alarm1 = alarms.GetValueOrDefault("KaRecipes.M01.DB_00_Parameters.single1");
+            var alarm2 = alarms.GetValueOrDefault("KaRecipes.M01.DB_00_Parameters.single2");
+            var alarm3 = alarms.GetValueOrDefault("KaRecipes.M01.DB_00_Parameters.single3");
+            //Assert
+            Assert.Equal(false, alarm1.Value);
+            Assert.Equal(true, alarm2.Value);
+            Assert.Equal(false, alarm3.Value);
+        }
+    }
+}
